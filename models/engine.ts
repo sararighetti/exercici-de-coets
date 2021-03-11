ab637a216d27faea7f94598e9f74708338fd0ae5
sararighetti <71082145+sararighetti@users.noreply.github.com>
2021-03-11 12:14:06 +0100

    Update engine.ts

--- models/engine.ts
+++ models/engine.ts
@@ -6,23 +6,11 @@ class Engine{
        this._maxSpeed = maxSpeed;
        this.speed = 0;
    }
-
-    get getmaxSpeed(): number{
-       return this._maxSpeed;
-    }
-    set setMaxSpeed(maxSpeed:number){
-       this._maxSpeed = maxSpeed;
-    }
-    get getSpeed(){
-       return this.speed;
-    }
-    set setSpeed(speed:number){
-       this.speed = speed;
-    }
   
     acceleraPropulsore(){    
      return this.speed +=10;
     }
+
     decelerate(){
      return this.speed += -10;
     }
