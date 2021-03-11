9b9354ef8fe39df08f2eee454298ac6f92577ce3
sararighetti <71082145+sararighetti@users.noreply.github.com>
2021-03-11 12:13:44 +0100

    Update engine.js

--- models/engine.js
+++ models/engine.js
@@ -4,34 +4,6 @@ var Engine = /** @class */ (function () {
         this._maxSpeed = maxSpeed;
         this.speed = 0;
     }
-    Object.defineProperty(Engine.prototype, "getmaxSpeed", {
-        get: function () {
-            return this._maxSpeed;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Engine.prototype, "setMaxSpeed", {
-        set: function (maxSpeed) {
-            this._maxSpeed = maxSpeed;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Engine.prototype, "getSpeed", {
-        get: function () {
-            return this.speed;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Engine.prototype, "setSpeed", {
-        set: function (speed) {
-            this.speed = speed;
-        },
-        enumerable: false,
-        configurable: true
-    });
     Engine.prototype.acceleraPropulsore = function () {
         return this.speed += 10;
     };
